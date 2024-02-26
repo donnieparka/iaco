5fabc10d60b32575d70d2a77e00e3ae7039fcb44
iaco <alberto.iacovella@gmail.com>
2024-02-25 22:22:01 -0800

    flash added

--- app.js
+++ app.js
@@ -1,66 +1,73 @@
 // Importa i moduli necessari
-import express from 'express';
-import path from 'path';
-import mongoose from 'mongoose';
-import ejsMate from 'ejs-mate';
-import flash from 'connect-flash';
-import session from 'express-session';
+import express from "express";
+import path from "path";
+import mongoose from "mongoose";
+import ejsMate from "ejs-mate";
+import flash from "connect-flash";
+import session from "express-session";
 // utils
-import asyncWrapper from './utils/asyncWrapper.js';
-import ExpressError from './utils/ExpressError.js';
-import { checkCampground, checkReview } from './utils/checkSchema.js';
-import methodMiddleware from './utils/methodMiddleware.js';
+import ExpressError from "./utils/ExpressError.js";
+import methodMiddleware from "./utils/methodMiddleware.js";
 // import dei router
-import campgroundsRouter from './routes/campgrounds.js';
-import reviewsRouter from './routes/reviews.js';
+import campgroundsRouter from "./routes/campgrounds.js";
+import reviewsRouter from "./routes/reviews.js";
 // Connette al database MongoDB
-mongoose.connect('mongodb://localhost:27017/yelp-camp-fake');
+mongoose.connect("mongodb://localhost:27017/yelp-camp-fake");
 
+/* dichiarazione variabili */
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-	console.log('Database connected');
+const app = express();
+const sessionSetup = {
+  secret: "dspoasdjfpaisj",
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
+
+/* connessione a mongodb */
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
 });
 
-const app = express();
-
 // Configurazione dell'app Express
-app.engine('ejs', ejsMate);
-app.set('view engine', 'ejs');
-app.set('views', path.join(process.cwd(), 'views'));
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(process.cwd(), "views"));
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(methodMiddleware);
-app.use(express.static('public'));
-app.use(
-	session({
-		secret: 'dspoasdjfpaisj',
-		resave: false,
-		saveUninitialized: false,
-	}),
-);
+app.use(express.static(path.join(process.cwd(), "public")));
+app.use(session(sessionSetup));
 app.use(flash());
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  next();
+});
 // Route per la pagina iniziale
-app.get('/', (req, res) => {
-	res.render('home');
+app.get("/", (req, res) => {
+  res.render("home");
 });
 
 // router
-app.use('/campgrounds', campgroundsRouter);
-app.use('/campgrounds/:id/reviews', reviewsRouter);
-app.all('*', (req, res, next) => {
-	const err = new ExpressError('qua non c`è un cazzo', 404);
-	next(err);
+app.use("/campgrounds", campgroundsRouter);
+app.use("/campgrounds/:id/reviews", reviewsRouter);
+app.all("*", (req, res, next) => {
+  const err = new ExpressError("qua non c`è un cazzo", 404);
+  next(err);
 });
 
 app.use((err, req, res, next) => {
-	if (!err.message) err.message = 'internal error';
-	if (!err.status) err.status = 500;
-	res.status(err.status).render('error', { err });
+  if (!err.message) err.message = "internal error";
+  if (!err.status) err.status = 500;
+  res.status(err.status).render("error", { err });
 });
 
 // Avvia il server Express sulla porta 3000
 app.listen(3000, () => {
-	console.log('Serving on port 3000');
+  console.log("Serving on port 3000");
 });
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,36 +1,39 @@
-import express from 'express';
+import express from "express";
+import flash from "connect-flash";
 const reviewsRouter = express.Router({ mergeParams: true });
 // models
-import Campground from '../models/campground.js';
-import Review from '../models/review.js';
+import Campground from "../models/campground.js";
+import Review from "../models/review.js";
 // utils
-import asyncWrapper from '../utils/asyncWrapper.js';
-import { checkReview } from '../utils/checkSchema.js';
+import asyncWrapper from "../utils/asyncWrapper.js";
+import { checkReview } from "../utils/checkSchema.js";
 
 // aggiunta review al campeggio
 reviewsRouter.post(
-	'/',
-	checkReview,
-	asyncWrapper(async (req, res) => {
-		const camp = await Campground.findById(req.params.id);
-		const review = new Review(req.body.review);
-		camp.reviews.push(review);
-		await camp.save();
-		await review.save();
-		res.redirect(`/campgrounds/${req.params.id}`);
-	}),
+  "/",
+  checkReview,
+  asyncWrapper(async (req, res) => {
+    const camp = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    camp.reviews.push(review);
+    await camp.save();
+    await review.save();
+    req.flash("success", "review Added!!");
+    res.redirect(`/campgrounds/${req.params.id}`);
+  })
 );
 
 // eliminazione review
 reviewsRouter.delete(
-	'/:revid',
-	asyncWrapper(async (req, res) => {
-		await Campground.findByIdAndUpdate(req.params.id, {
-			$pull: { reviews: req.params.revid },
-		});
-		await Review.findByIdAndDelete(req.params.revid);
-		res.redirect(`/campgrounds/${req.params.id}`);
-	}),
+  "/:revid",
+  asyncWrapper(async (req, res) => {
+    await Campground.findByIdAndUpdate(req.params.id, {
+      $pull: { reviews: req.params.revid },
+    });
+    await Review.findByIdAndDelete(req.params.revid);
+    req.flash("success", "review removed!!");
+    res.redirect(`/campgrounds/${req.params.id}`);
+  })
 );
 
 export default reviewsRouter;
