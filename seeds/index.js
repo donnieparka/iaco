5a273d97f6f6bc03cb71e1df836a1b07470c7a63
iaco <alberto.iacovella@gmail.com>
2024-04-18 15:42:07 +0200

    rimossa localizzazione manuale nel seed perché pesante

--- seeds/index.js
+++ seeds/index.js
@@ -2,43 +2,71 @@ import cities from './cities.js';
 import { places, descriptors } from './seedHelpers.js';
 import { Campground } from '../mongooseModels.js';
 import { GeoCoder } from '../utils/mapBox.js';
-const geoCoder = new GeoCoder();
+// const geoCoder = new GeoCoder();
 
 const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
-	const apiToken = process.env.MAPBOX_TOKEN;
+	// const apiToken = process.env.MAPBOX_TOKEN;
 	await Campground.deleteMany({});
-	for (let i = 0; i < 50; i++) {
-		const random1000 = Math.floor(Math.random() * 1000);
-		const price = Math.floor(Math.random() * 20) + 10;
-		const coordinates = await geoCoder.localize(
-			apiToken,
-			`${cities[random1000].city} ${cities[random1000].state}`
-		);
-		coordinates.reverse();
-		const camp = new Campground({
-			location: `${cities[random1000].city}, ${cities[random1000].state}`,
-			author: '65e85112bcf372d2643f013b',
-			// author: '6614f6cd6221f6d6ddc9f4b3',
-			title: `${sample(descriptors)} ${sample(places)}`,
-			geometry: {
-				type: 'Point',
-				coordinates: coordinates,
-			},
-			images: [
-				{
+	 for (let i = 0; i < 300; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
+        const camp = new Campground({
+			//YOUR USER ID
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+			// author: '65e85112bcf372d2643f013b',
+			author: '6614f6cd6221f6d6ddc9f4b3',
+            title: `${sample(descriptors)} ${sample(places)}`,
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
+            price,
+            geometry: {
+                type: "Point",
+                coordinates: [
+                    cities[random1000].longitude,
+                    cities[random1000].latitude,
+                ]
+            },
+            images: [
+                {
 					url: 'https://res.cloudinary.com/dsntv0w1a/image/upload/v1710413918/samples/people/kitchen-bar.jpg',
-					filename: 'b4vzw2dhewcoqe3b4fyu',
-					_id: '65f9f35308955ee4ca51e1e0',
-				},
-			],
+                    filename: 'YelpCamp/ahfnenvca4tha00h2ubt'
+                },
+                {
+					url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ahfnenvca4tha00h2ubt.png',
+                    filename: 'YelpCamp/ruyoaxgf72nzpi4y6cdi'
+                }
+            ]
+        })
+        await camp.save();
+	// for (let i = 0; i < 50; i++) {
+	// 	const random1000 = Math.floor(Math.random() * 1000);
+	// 	const price = Math.floor(Math.random() * 20) + 10;
+	// 	const coordinates = await geoCoder.localize(
+	// 		apiToken,
+	// 		`${cities[random1000].city} ${cities[random1000].state}`
+	// 	);
+	// 	coordinates.reverse();
+	// 	const camp = new Campground({
+	// 		location: `${cities[random1000].city}, ${cities[random1000].state}`,
+	// 		title: `${sample(descriptors)} ${sample(places)}`,
+	// 		geometry: {
+	// 			type: 'Point',
+	// 			coordinates: coordinates,
+	// 		},
+	// 		images: [
+	// 			{
+	// 				url: '',
+	// 				filename: 'b4vzw2dhewcoqe3b4fyu',
+	// 				_id: '65f9f35308955ee4ca51e1e0',
+	// 			},
+	// 		],
 
-			description:
-				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
-			price,
-		});
-		await camp.save();
+	// 		description:
+	// 			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
+	// 		price,
+	// 	});
+	// 	await camp.save();
 	}
 };
 
